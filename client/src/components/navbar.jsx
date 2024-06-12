55544abcd2cc00733adb70639e80ee4ebff5b769
SemEtic17 <semetic17@gmail.com>
2024-06-12 19:09:13 +0300

    Catagorizing the movies and coloring the navbar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -4,7 +4,7 @@ import d from '../assets/dark.svg';
 export default function Navbarr({submit, change, sidebardata}) {
   return (
     <div>
-    <Navbar className="bg-sky-50 fixed top-1 w-[96%] shadow-md z-50" fluid rounded>
+    <Navbar className="bg-[#C2C4D2] fixed top-3 w-[96%] shadow-md z-50" fluid rounded>
       <Navbar.Brand>
         <img src={d} className="mr-3 h-6 sm:h-9 rounded" alt="MT Logo"  />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Movie Trailer</span>
@@ -13,10 +13,12 @@ export default function Navbarr({submit, change, sidebardata}) {
 <form onSubmit={submit} className="flex flex-row space-x-2">
       <Select onChange={change} value={sidebardata} id='category' className='rounded-none' size="sm">
       <option value='action'>Action</option>
-      <option value='romance'>Romance</option>
+      <option value='adventure'>Adventure</option>
+      <option value='animation'>Animation</option>
       <option value='comedy'>Comedy</option>
-      <option value='horror'>Horror</option>
+      <option value='crime'>Crime</option>
       <option value='fantasy'>Fantasy</option>
+      <option value='romance'>Romance</option>
       <option value='uncategorized'>uncategorized</option>
     </Select>
     <TextInput type='text' placeholder='Search...' rightIcon={AiOutlineSearch} id='searchTerm' onChange={change} />
